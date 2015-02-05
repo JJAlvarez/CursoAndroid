8bfbf92d295139a1400254d45756278a2a297fc2
ajchanquin <ajchanquin@gmail.com>
2015-02-05 09:54:41 -0600

    Adding information to SaludoActivity

--- PrimerProyecto/app/src/main/java/gt/org/kinal/primerproyecto/SaludoActivity.java
+++ PrimerProyecto/app/src/main/java/gt/org/kinal/primerproyecto/SaludoActivity.java
@@ -4,14 +4,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class SaludoActivity extends ActionBarActivity {
+
+    private TextView txtSaludo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saludo);
+        //Localizamos los controles
+        txtSaludo = (TextView)findViewById(R.id.TxtSaludo);
+        //Recuperamos la informacion pasada en el intent
+        Bundle bundle = this.getIntent().getExtras();
+        //Construimos el mensaje a mostrar
+        txtSaludo.setText("Hola " + bundle.getString("NombreIngresado"));
     }
 
 
